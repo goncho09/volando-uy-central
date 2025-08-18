49c30fc841d31918a68a000a392a35de7b75147d
unknown <unusuariodegogle.xd@gmail.com>
2025-08-18 10:12:40 -0300

    menuPrueba created

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,62 @@ import datatypes.DtFecha;
 import clases.ISistema;
 import clases.Factory;
 
-public class Main {
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class Main extends JFrame {
+
+    private JPanel menuPrincipal;
+    private JTabbedPane tabbedPane1;
+    private JPanel opcionesMenu;
+    private JButton panel1Button;
+    private JButton panel2Button;
+    private JButton panel4Button;
+    private JButton panel3Button;
+
+    public Main(){
+
+        add(menuPrincipal);
+
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(800,600);
+        setVisible(true);
+        setResizable(false);
+        setLocationRelativeTo(null);
+
+
+
+        panel1Button.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                tabbedPane1.setSelectedIndex(0);
+            }
+        });
+        panel2Button.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                tabbedPane1.setSelectedIndex(1);
+            }
+        });
+        panel3Button.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                tabbedPane1.setSelectedIndex(2);
+            }
+        });
+        panel4Button.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                tabbedPane1.setSelectedIndex(3);
+            }
+        });
+    }
+
     public static void main(String[] args) {
+
+        new Main();
+
         System.out.println("hgoaññ");
         DtFecha fecha = new DtFecha(1,4,2003);
         DtFecha fecha2 = new DtFecha(2,4,2003);
