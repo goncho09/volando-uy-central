5f6ebd80a41a7ef7463565cb0ac537568af1c752
goncho09 <gonzalolarricaV2@gmail.com>
2025-08-18 15:14:48 -0300

    FEAT(not completed): add function consultaPaqueteRutasVuelo

--- src/clases/ISistema.java
+++ src/clases/ISistema.java
@@ -2,8 +2,21 @@ package clases;
 
 import datatypes.DtCategoria;
 import datatypes.DtCiudad;
+import datatypes.DtPaquete;
+
+import java.util.LinkedHashMap;
+import java.util.List;
 
 public interface ISistema {
+    List<DtPaquete> listarPaquetes();
+    void seleccionarPaquete(String nombre);
+    DtPaquete getPaquete();
+
     void altaCategoria(DtCategoria categoria);
     void altaCiudad(DtCiudad ciudad);
+
+    // TEMPORAL
+    List<Ciudad> getCiudades();
+    void altaPaquete(DtPaquete paquete);
+    List<Categoria> getCategorias();
 }
