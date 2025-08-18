6fb3f46ff179be56b8350c27a8ccdfcfb4b9387f
VisualZac <144742015+VisualZac@users.noreply.github.com>
2025-08-18 14:17:49 -0300

    Consulta de vuelo falta por implementar las rutas

--- src/clases/Sistema.java
+++ src/clases/Sistema.java
@@ -2,6 +2,7 @@ package clases;
 
 import datatypes.DtCategoria;
 import datatypes.DtCiudad;
+import datatypes.DtVuelo;
 
 import java.util.LinkedHashMap;
 import java.util.Map;
@@ -10,6 +11,7 @@ public class Sistema implements ISistema {
     private static Sistema instancia;
     private Map<String, Categoria> categorias;
     private Map<String, Ciudad> ciudades;
+    private Map<String, Vuelo> vuelos;
 
     private Sistema() {
         this.categorias = new LinkedHashMap<>();
@@ -35,5 +37,12 @@ public class Sistema implements ISistema {
         if(existe != null ) { throw new IllegalArgumentException("Ya existe una ciudad con ese nombre.");}
         Ciudad c = new Ciudad(ciudad);
         this.ciudades.put(c.getNombre(),c);
+    }
+
+    public void consultaVuelo(DtVuelo vuelo) {
+        Vuelo v = this.vuelos.get(vuelo.getNombre());
+        if (v == null) {
+            throw new IllegalArgumentException("Vuelo no encontrado.");
+        }
     }
 }
