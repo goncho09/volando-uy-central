a2f9ce61f3dddf69045e6f148b98c73b61223f14
goncho09 <gonzalolarricaV2@gmail.com>
2025-08-18 11:53:11 -0300

    FEAT: add altaCategoria function

--- src/clases/Sistema.java
+++ src/clases/Sistema.java
@@ -1,9 +1,16 @@
 package clases;
 
+import datatypes.DtCategoria;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
 public class Sistema implements ISistema {
     private static Sistema instancia;
+    private Map<String, Categoria> categorias;
 
-    private Sistema() { }
+    private Sistema() {
+        this.categorias = new LinkedHashMap<>();
+    }
 
     public static Sistema getInstancia() {
         if (instancia == null) {
@@ -15,5 +22,13 @@ public class Sistema implements ISistema {
     @Override
     public void ejecutar() {
         System.out.println("Sistema ejecutando...");
+    }
+
+    public void altaCategoria(DtCategoria categoria){
+        Categoria existe = this.categorias.get(categoria.getNombre());
+        if(existe != null ) { throw new IllegalArgumentException("Ya existe una categoría con ese nombre.");}
+        Categoria c = new Categoria(categoria);
+        this.categorias.put(c.getNombre(),c);
+
     }
 }
