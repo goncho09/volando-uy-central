c1b3f8a96c61e72f5472b5110d726d39fa2dae3f
Gonzalo Larrica <76012483+goncho09@users.noreply.github.com>
2025-10-08 17:01:27 -0300

    add show password

--- src/main/java/com/app/clases/Usuario.java
+++ src/main/java/com/app/clases/Usuario.java
@@ -1,89 +1,157 @@
 package com.app.clases;
 
-import com.app.datatypes.DtUsuario;
+import com.app.datatypes.*;
+import com.app.enums.*;
 import jakarta.persistence.*;
 
+import java.time.LocalDate;
+import java.util.ArrayList;
+
+import java.util.List;
 
 @Entity
-@Inheritance(strategy = InheritanceType.JOINED) // Esto indica que es una herencia, por ende tendrá subclases, utilizará el método Join para crear las "subtablas"
-@DiscriminatorColumn(name = "userType") // Indica que creará una columna para diferenciar las clases. El valor depende de las subclases.
-public abstract class Usuario {
-    @Id protected String nickname; // Indica que nickname será la id
+@DiscriminatorValue("Cliente") // Cuando se haga la tabla de usuarios, éste en particular será "userType" Cliente
+public class Cliente extends Usuario{
+    private String apellido;
+    private LocalDate fechaNacimiento;
+    private String nacionalidad;
+    private TipoDocumento tipoDocumento;
+    private int numeroDocumento;
 
-    @Column(nullable = false, length = 50) // Indica que será una columna no nula
-    protected String nombre;
+    @OneToMany(mappedBy = "cliente",cascade = CascadeType.ALL, orphanRemoval = true)// 1 - N (un cliente tiene muchas "compras"
+    private List<CompraPaquete> comprasPaquetes = new ArrayList<>();
 
-    @Column(nullable = false, unique = true)
-    protected String email;
+    @OneToMany(mappedBy = "cliente") // 1 - N (un cliente tiene muchas "reservas"
+    private List<Reserva> reservas = new ArrayList<>();
 
-    @Column(nullable = false)
-    protected String password;
-
-    @Column(nullable = false)
-    protected String urlImage;
-
-    public Usuario() {}
-    public Usuario(DtUsuario usuario){
-        this.nickname = usuario.getNickname();
-        this.nombre = usuario.getNombre();
-        this.email = usuario.getEmail();
-        this.password = usuario.getPassword();
-        this.urlImage = usuario.getUrlImage();
+    public Cliente(){}
+    public Cliente(DtCliente cliente) {
+        super(new DtUsuario(cliente.getNickname(), cliente.getNombre(), cliente.getEmail(), cliente.getPassword(), cliente.getUrlImage()));
+        this.apellido = cliente.getApellido();
+        this.fechaNacimiento = cliente.getFechaNacimiento();
+        this.nacionalidad = cliente.getNacionalidad();
+        this.tipoDocumento = cliente.getTipoDocumento();
+        this.numeroDocumento = cliente.getNumeroDocumento();
+        this.comprasPaquetes = new ArrayList<>();
+        this.reservas = new ArrayList<>();
     }
 
-    public String getNickname() {
-        return nickname;
+    public String getApellido() {
+        return apellido;
     }
 
-    public String getNombre() {
-        return nombre;
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
     }
 
-    public String getEmail() {
-        return email;
+    public LocalDate getFechaNacimiento() {
+        return fechaNacimiento;
     }
 
-    public String getPassword() {
-        return password;
+    public void setFechaNacimiento(LocalDate fechaNacimiento) {
+        this.fechaNacimiento = fechaNacimiento;
     }
 
-    public String getUrlImage() {
-        return urlImage;
+    public String getNacionalidad() {
+        return nacionalidad;
     }
 
-    public void setNickname(String nickname) {
-        this.nickname = nickname;
+    public void setNacionalidad(String nacionalidad) {
+        this.nacionalidad = nacionalidad;
     }
 
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
+    public TipoDocumento getTipoDocumento() {
+        return tipoDocumento;
     }
 
-    public void setEmail(String email) {
-        this.email = email;
+    public void setTipoDocumento(TipoDocumento tipoDocumento) {
+        this.tipoDocumento = tipoDocumento;
     }
 
-    public void setPassword(String password) {
-        this.password = password;
+    public int getNumeroDocumento() {
+        return numeroDocumento;
     }
 
-    public void setUrlImage(String urlImage) {
-        this.urlImage = urlImage;
+    public void setNumeroDocumento(int numeroDocumento) {
+        this.numeroDocumento = numeroDocumento;
     }
 
-    public DtUsuario getDatos() {
-        return new DtUsuario(
-                this.nickname,
-                this.nombre,
-                this.email,
-                this.password,
-                this.urlImage
+    public List<CompraPaquete> getComprasPaquetes() {
+        return comprasPaquetes;
+    }
+
+    public void setComprasPaquetes(List<CompraPaquete> comprasPaquetes) {
+        this.comprasPaquetes = comprasPaquetes;
+    }
+
+    public List<Reserva> getReservas(){
+        return this.reservas;
+    }
+
+    public List<DtReserva> getDtReservas() {
+        List<DtReserva> dtReservas = new ArrayList<>();
+        for (Reserva reserva : this.reservas) {
+            dtReservas.add(new DtReserva(
+                    reserva.getFecha(),
+                    reserva.getTipoAsiento(),
+                    reserva.getCantPasajes(),
+                    reserva.getEquipajeExtra(),
+                    reserva.getCosto(),
+                    reserva.getPasajeros(),
+                    this,  // El cliente actual
+                    reserva.getVuelo(),
+                    reserva.getPaquete()
+            ));
+        }
+        return dtReservas;
+    }
+
+    public List<DtPaquete> getPaquetesAdquiridos() {
+        List<DtPaquete> dtPaquetes = new ArrayList<>();
+        if (comprasPaquetes != null) {
+            for (CompraPaquete compra : comprasPaquetes) {
+                Paquete paquete = compra.getPaquete();
+                dtPaquetes.add(new DtPaquete(
+                        paquete.getNombre(),
+                        paquete.getDescripcion(),
+                        paquete.getValidezDias(),
+                        paquete.getDescuento(),
+                        paquete.getCosto(),
+                        paquete.getRutaEnPaquete()
+                ));
+            }
+        }
+        return dtPaquetes;
+    }
+
+    public DtCliente getDatos(){
+        return new DtCliente(
+                this.getNickname(),
+                this.getNombre(),
+                this.getEmail(),
+                this.getPassword(),
+                this.getUrlImage(),
+                this.getApellido(),
+                this.getFechaNacimiento(),
+                this.getNacionalidad(),
+                this.getTipoDocumento(),
+                this.getNumeroDocumento()
         );
     }
 
-    @Override
-    public String toString() {
-        return this.nickname;
+    public boolean existeVueloReserva(Vuelo vuelo){
+        for(Reserva r : this.getReservas()){
+            if(r.getVuelo().getNombre().equals(vuelo.getNombre())){
+                return true;
+            }
+        }
+        return false;
+    };
+
+    public void mostrarDatos() {
+        System.out.println("Datos Usuario: " +
+                nickname + " - " + nombre + " - " + apellido + " - " +
+                email + " - " + fechaNacimiento + " - " + nacionalidad);
     }
 }
 
