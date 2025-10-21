7eef16d1335a31e54e71b61a5086d6703ed32d2b
unknown <unusuariodegogle.xd@gmail.com>
2025-10-21 14:34:40 -0300

    merge

--- src/main/java/com/app/Main.java
+++ src/main/java/com/app/Main.java
@@ -41,7 +41,7 @@ public class Main extends JFrame {
     private JSpinner JSpinnerTuristasAltaVuelo;
     private JSpinner JSpinnerEjecutivosAltaVuelo;
     private JSpinner JSpinnerDiaAltaVuelo;
-    private JSpinner JSpinnerAñoAltaVuelo;
+    private JSpinner JSpinnerAnioAltaVuelo;
     private JSpinner JSpinnerMesAltaVuelo;
     private JComboBox JComboBoxAerolineaAltaVuelo;
     private JPanel formVuelo;
@@ -280,7 +280,7 @@ public class Main extends JFrame {
 
         JSpinnerMesAltaVuelo.setModel(new SpinnerNumberModel(1, 1, 12, 1));
 
-        JSpinnerAñoAltaVuelo.setModel(new SpinnerNumberModel(2025, 2025, 2030, 1));
+        JSpinnerAnioAltaVuelo.setModel(new SpinnerNumberModel(2025, 2025, 2030, 1));
 
         JSpinnerEjecutivosAltaVuelo.setModel(new SpinnerNumberModel(1, 1, 100, 1));
         JSpinnerTuristasAltaVuelo.setModel(new SpinnerNumberModel(1, 1, 100, 1));
@@ -702,7 +702,7 @@ public class Main extends JFrame {
 
                     auxiliar.validarNombreVuelo(nombre);
 
-                    LocalDate fecha = LocalDate.of((Integer)JSpinnerAñoAltaVuelo.getValue(), (Integer)JSpinnerMesAltaVuelo.getValue(), (Integer)JSpinnerDiaAltaVuelo.getValue());
+                    LocalDate fecha = LocalDate.of((Integer) JSpinnerAnioAltaVuelo.getValue(), (Integer)JSpinnerMesAltaVuelo.getValue(), (Integer)JSpinnerDiaAltaVuelo.getValue());
                     LocalTime hora = LocalTime.of((Integer) JSpinnerDuracionAltaVueloHora.getValue(), (Integer) JSpinnerDuracionAltaVueloMinuto.getValue());
 
                     if(fecha.isBefore(LocalDate.now())){
@@ -728,11 +728,7 @@ public class Main extends JFrame {
                     DtVuelo dtVuelo = new DtVuelo(nombre, fecha, hora, (Integer)JSpinnerTuristasAltaVuelo.getValue(), (Integer)JSpinnerEjecutivosAltaVuelo.getValue(), urlImage, LocalDate.now(), null, 0);
 
                     try {
-                        s.seleccionarAerolineaParaVuelo(aerolinea);
-                        s.seleccionarRuta(ruta);
-                        s.ingresarDatosVuelo(dtVuelo);
-                        s.confirmarAltaVuelo();
-                        s.consultarVuelo(dtVuelo.getNombre());
+                        s.altaVuelo(dtVuelo);
                         new VentanaMensaje("Vuelo creado correctamente");
                     } catch (IllegalArgumentException ex) {
                         AuxiliarFunctions.borrarImagen(urlImage, TipoImagen.VUELO);
