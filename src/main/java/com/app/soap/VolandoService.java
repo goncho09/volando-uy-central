5bf9a3e9d5562aab877752d073becd2ae4a173a8
unknown <unusuariodegogle.xd@gmail.com>
2025-11-16 15:25:16 -0300

    metodos servibles SJFKAS (ya se puede consumir por el WAR)

--- src/main/java/com/app/soap/VolandoService.java
+++ src/main/java/com/app/soap/VolandoService.java
@@ -1,9 +1,13 @@
 package com.app.soap;
 
+import javax.jws.WebMethod;
 import javax.jws.WebService;
 import com.app.clases.Factory;
 import com.app.clases.ISistema;
-import com.app.datatypes.DtVuelo;
+import com.app.datatypes.*;
+
+import java.time.LocalDate;
+import java.util.List;
 
 @WebService(endpointInterface = "com.app.soap.VolandoServicePort")
 public class VolandoService implements VolandoServicePort {
@@ -12,16 +16,262 @@ public class VolandoService implements VolandoServicePort {
 
     // Aquí solamente se deben "publicar" las funciones que se utilizarán por fuera de la app
 
+    // ---------- USUARIOS ---------- //
+
     @Override
-    public String ping() {
-        return "Volando SOAP OK";
+    public boolean existeUsuario(String nickname) {
+        return sistema.existeUsuario(nickname);
     }
 
     @Override
-    public String obtenerVuelo(String nombreVuelo) {
-        DtVuelo v = sistema.getVuelo(nombreVuelo);
-        return (v != null) ? v.toString() : "No existe";
+    public boolean existeUsuarioEmail(String email) {
+        return sistema.existeUsuarioEmail(email);
     }
+
+    @Override
+    public boolean validarUsuario(String nickname, String password) {
+        return sistema.validarUsuario(nickname, password);
+    }
+
+    @Override
+    public void registrarAerolinea(DtAerolinea aerolinea) {
+        sistema.registrarAerolinea(aerolinea);
+    }
+
+    @Override
+    public void modificarAerolinea(DtAerolinea aerolinea) {
+        sistema.modificarAerolinea(aerolinea);
+    }
+
+    @Override
+    public void modificarAerolineaImagen(DtAerolinea aerolinea, String urlImagen) {
+        sistema.modificarAerolineaImagen(aerolinea, urlImagen);
+    }
+
+    @Override
+    public void registrarCliente(DtCliente cliente) {
+        sistema.registrarCliente(cliente);
+    }
+
+    @Override
+    public void modificarCliente(DtCliente cliente) {
+        sistema.modificarCliente(cliente);
+    }
+
+    @Override
+    public void modificarClienteImagen(DtCliente cliente, String urlImagen) {
+        sistema.modificarClienteImagen(cliente, urlImagen);
+    }
+
+    @Override
+    public DtUsuario getUsuario(String nickname) {
+        return sistema.getUsuario(nickname);
+    }
+
+    @Override
+    public DtCliente getCliente(String nickname) {
+        return sistema.getCliente(nickname);
+    }
+
+    @Override
+    public DtAerolinea getAerolinea(String nickname) {
+        return sistema.getAerolinea(nickname);
+    }
+
+    @Override
+    public List<DtUsuario> listarUsuarios() {
+        return sistema.listarUsuarios();
+    }
+
+    @Override
+    public List<DtAerolinea> listarAerolineas() {
+        return sistema.listarAerolineas();
+    }
+
+    @Override
+    public void seguirUsuario(String usuarioSeguidor, String usuarioASeguir) {
+        sistema.seguirUsuario(usuarioSeguidor, usuarioASeguir);
+    }
+
+    @Override
+    public void dejarDeSeguirUsuario(String usuarioSeguidor, String usuarioASeguir) {
+        sistema.dejarDeSeguirUsuario(usuarioSeguidor, usuarioASeguir);
+    }
+
+// ------------ RESERVAS ----------- //
+
+    @Override
+    public void altaReserva(DtReserva reserva) {
+        sistema.altaReserva(reserva);
+    }
+
+    @Override
+    public DtReserva getReservaCliente(DtVuelo vuelo, DtCliente cliente, LocalDate fechaReserva) {
+        return sistema.getReservaCliente(vuelo, cliente, fechaReserva);
+    }
+
+    @Override
+    public DtReserva getReservaAerolinea(DtVuelo vuelo, DtAerolinea aerolinea, LocalDate fechaReserva) {
+        return sistema.getReservaAerolinea(vuelo, aerolinea, fechaReserva);
+    }
+
+    @Override
+    public List<DtReserva> listarReservasCliente(String nickname) {
+        return sistema.listarReservasCliente(nickname);
+    }
+
+    @Override
+    public List<DtReserva> listarReservasAerolinea(String nickname) {
+        return sistema.listarReservasAerolinea(nickname);
+    }
+
+    @Override
+    public List<DtReserva> listarReservasClienteVuelo(DtCliente cliente, DtVuelo vuelo) {
+        return sistema.listarReservasClienteVuelo(cliente, vuelo);
+    }
+
+// ---------- RUTAS DE VUELO ---------- //
+
+    @Override
+    public boolean rutaContieneCategoria(DtRuta ruta, String categoria) {
+        return sistema.rutaContieneCategoria(ruta, categoria);
+    }
+
+    @Override
+    public boolean existeRuta(String nombre) {
+        return sistema.existeRuta(nombre);
+    }
+
+    @Override
+    public void altaRutaDeVuelo(String nickname, DtRuta datosRuta) {
+        sistema.altaRutaDeVuelo(nickname, datosRuta);
+    }
+
+    @Override
+    public DtRuta getRutaDeVuelo(String nombre) {
+        return sistema.getRutaDeVuelo(nombre);
+    }
+
+    @Override
+    public List<DtRuta> listarRutasDeVuelo() {
+        return sistema.listarRutasDeVuelo();
+    }
+
+// ---------- VUELOS ---------- //
+
+    @Override
+    public boolean existeVuelo(String nombre) {
+        return sistema.existeVuelo(nombre);
+    }
+
+    @Override
+    public void altaVuelo(DtVuelo vuelo) {
+        sistema.altaVuelo(vuelo);
+    }
+
+    @Override
+    public DtVuelo getVuelo(String nombre) {
+        return sistema.getVuelo(nombre);
+    }
+
+    @Override
+    public List<DtVuelo> listarVuelosRuta(String nombreRuta) {
+        return sistema.listarVuelosRuta(nombreRuta);
+    }
+
+    @Override
+    public List<DtVuelo> listarVuelosRutaFecha(String nombreRuta, LocalDate fecha) {
+        return sistema.listarVuelosRutaFecha(nombreRuta, fecha);
+    }
+
+    @Override
+    public List<DtVuelo> getVuelosRutaDeVuelo(DtRuta ruta) {
+        return sistema.getVuelosRutaDeVuelo(ruta);
+    }
+
+// ---------- PAQUETES ---------- //
+
+    @Override
+    public boolean estaPaqueteComprado(DtPaquete paquete) {
+        return sistema.estaPaqueteComprado(paquete);
+    }
+
+    @Override
+    public boolean existePaquete(String nombre) {
+        return sistema.existePaquete(nombre);
+    }
+
+    @Override
+    public void altaPaquete(DtPaquete paquete) {
+        sistema.altaPaquete(paquete);
+    }
+
+    @Override
+    public DtPaquete getPaquete(String nombre) {
+        return sistema.getPaquete(nombre);
+    }
+
+    @Override
+    public List<DtPaquete> listarPaquetes() {
+        return sistema.listarPaquetes();
+    }
+
+    @Override
+    public List<DtPaquete> listarPaquetesCliente(String nickname) {
+        return sistema.listarPaquetesCliente(nickname);
+    }
+
+    @Override
+    public List<DtPaquete> listarPaquetesAerolinea(String nickname) {
+        return sistema.listarPaquetesAerolinea(nickname);
+    }
+
+    @Override
+    public List<DtPaquete> listarPaquetesNoComprados() {
+        return sistema.listarPaquetesNoComprados();
+    }
+
+// ---------- CATEGORIAS ---------- //
+
+    @Override
+    public List<DtCategoria> listarCategorias() {
+        return sistema.listarCategorias();
+    }
+
+    @Override
+    public List<DtCategoria> buscarCategorias() {
+        return sistema.buscarCategorias();
+    }
+
+    @Override
+    public List<DtCategoria> buscarCategoriasPorNombre(List<String> nombres) {
+        return sistema.buscarCategoriasPorNombre(nombres);
+    }
+
+// ---------- CIUDADES ---------- //
+
+    @Override
+    public void altaCiudad(DtCiudad ciudad) {
+        sistema.altaCiudad(ciudad);
+    }
+
+    @Override
+    public DtCiudad getCiudad(String nombre, String pais) {
+        return sistema.getCiudad(nombre, pais);
+    }
+
+    @Override
+    public List<DtCiudad> listarCiudades() {
+        return sistema.listarCiudades();
+    }
+
+// ---------- COMPRAS ---------- //
+
+    @Override
+    public void compraPaquete(DtPaquete paquete, DtCliente cliente) {
+        sistema.compraPaquete(paquete, cliente);
+    }
+
 
 
 }
--- src/main/java/com/app/soap/VolandoServicePort.java
+++ src/main/java/com/app/soap/VolandoServicePort.java
@@ -1,14 +1,208 @@
 package com.app.soap;
 
+import java.time.LocalDate;
+import java.util.List;
+
+import com.app.datatypes.DtAerolinea;
+import com.app.datatypes.DtCliente;
+import com.app.datatypes.DtReserva;
+import com.app.datatypes.DtUsuario;
+import com.app.datatypes.DtVuelo;
+import com.app.datatypes.DtRuta;
+import com.app.datatypes.DtPaquete;
+import com.app.datatypes.DtCiudad;
+import com.app.datatypes.DtCategoria;
+
 import javax.jws.WebMethod;
 import javax.jws.WebService;
 
 @WebService
 public interface VolandoServicePort {
 
+    //Aquí están SOLO los métodos que queremos "compartir" o dar como servicio a nuestra web.
+
+    // ---------- USUARIOS ---------- //
     @WebMethod
-    String ping();
+    boolean existeUsuario(String nickname);
 
     @WebMethod
-    String obtenerVuelo(String nombreVuelo);
+    boolean existeUsuarioEmail(String email);
+
+//    boolean clienteTienePaquete(String nickname, String nombrePaquete);
+
+    @WebMethod
+    boolean validarUsuario(String nickname, String password);
+
+//    boolean aerolineaTieneRuta(DtAerolinea aerolinea, String nombre);
+
+    @WebMethod
+    void registrarAerolinea(DtAerolinea aerolinea);
+
+    @WebMethod
+    void modificarAerolinea(DtAerolinea aerolinea);
+
+    @WebMethod
+    void modificarAerolineaImagen(DtAerolinea aerolinea, String urlImagen);
+
+    @WebMethod
+    void registrarCliente(DtCliente cliente);
+
+    @WebMethod
+    void modificarCliente(DtCliente cliente);
+
+    @WebMethod
+    void modificarClienteImagen(DtCliente cliente, String urlImagen);
+
+//    Aerolinea buscarAerolinea(String nickname);
+//    Cliente buscarCliente(String nickname);
+
+    @WebMethod
+    DtUsuario getUsuario(String nickname);
+
+    @WebMethod
+    DtCliente getCliente(String nickname);
+
+    @WebMethod
+    DtAerolinea getAerolinea(String nickname);
+
+    @WebMethod
+    List<DtUsuario> listarUsuarios();
+
+//    List<DtCliente> listarClientes();
+
+    @WebMethod
+    List<DtAerolinea> listarAerolineas();
+
+    @WebMethod
+    void seguirUsuario(String usuarioSeguidor, String usuarioASeguir);
+
+    @WebMethod
+    void dejarDeSeguirUsuario(String usuarioSeguidor, String usuarioASeguir);
+
+
+    // ------------ RESERVAS ----------- //
+    @WebMethod
+    void altaReserva(DtReserva reserva); //Reserva de un Vuelo
+
+//    DtReserva buscarReserva(DtReserva reserva);
+
+    @WebMethod
+    DtReserva getReservaCliente(DtVuelo vuelo, DtCliente cliente, LocalDate fechaReserva);
+
+    @WebMethod
+    DtReserva getReservaAerolinea(DtVuelo vuelo, DtAerolinea aerolinea, LocalDate fechaReserva);
+
+//    List<DtReserva> listarReservasVuelo(String nombreVuelo); // Devuelve las reservas de un vuelo en específico
+
+    @WebMethod
+    List<DtReserva> listarReservasCliente(String nickname); // Devuelve las reservas de un cliente en específico
+
+    @WebMethod
+    List<DtReserva> listarReservasAerolinea(String nickname); // Devuelve las reservas de una aerolínea en específico
+
+    @WebMethod
+    List<DtReserva> listarReservasClienteVuelo(DtCliente cliente, DtVuelo vuelo); // Devuelve las reservas de un cliente en un vuelo en específico
+
+
+    // ---------- RUTAS DE VUELO ---------- //
+    @WebMethod
+    boolean rutaContieneCategoria(DtRuta ruta, String categoria);
+
+    @WebMethod
+    boolean existeRuta(String nombre);
+
+//    void actualizarEstadoRuta(DtRuta ruta, EstadoRuta estado);
+
+    @WebMethod
+    void altaRutaDeVuelo(String nickname, DtRuta datosRuta);
+
+    @WebMethod
+    DtRuta getRutaDeVuelo(String nombre);
+
+    @WebMethod
+    List<DtRuta> listarRutasDeVuelo(); // Devuelve TODAS las rutas de vuelo
+
+//    List<DtRuta> listarRutasDeVueloPaquete(String paquete); // Devuelve TODAS las rutas de un paquete en específico
+
+    // ---------- VUELOS ---------- //
+    @WebMethod
+    boolean existeVuelo(String nombre);
+
+    @WebMethod
+    void altaVuelo(DtVuelo vuelo);
+
+    @WebMethod
+    DtVuelo getVuelo(String nombre);
+
+//    List<DtVuelo> listarVuelos();
+
+    @WebMethod
+    List<DtVuelo> listarVuelosRuta(String nombreRuta); // Busca vuelos por nombre de ruta
+
+    @WebMethod
+    List<DtVuelo> listarVuelosRutaFecha(String nombreRuta, LocalDate fecha); // Busca vuelos por nombre de ruta y fecha opcional
+
+    @WebMethod
+    List<DtVuelo> getVuelosRutaDeVuelo(DtRuta ruta);
+
+    // ---------- PAQUETES ---------- //
+    @WebMethod
+    boolean estaPaqueteComprado(DtPaquete paquete);
+
+    @WebMethod
+    boolean existePaquete(String nombre);
+
+//    int agregarRutaAPaquete(DtPaquete paquete, DtRuta ruta, int cantidad, TipoAsiento tipoAsiento);
+
+    @WebMethod
+    void altaPaquete(DtPaquete paquete);
+
+    @WebMethod
+    DtPaquete getPaquete(String nombre);
+
+    @WebMethod
+    List<DtPaquete> listarPaquetes();
+
+    @WebMethod
+    List<DtPaquete> listarPaquetesCliente(String nickname); // Lista todos los paquetes comprados por un cliente.
+
+    @WebMethod
+    List<DtPaquete> listarPaquetesAerolinea(String nickname);
+
+    @WebMethod
+    List<DtPaquete> listarPaquetesNoComprados();
+
+//    List<DtPaquete> listarPaquetesConRutas();
+
+
+    // ---------- CATEGORIAS ---------- //
+//    void altaCategoria(DtCategoria categoria);
+//    DtCategoria getCategoria(String nombre);
+
+    @WebMethod
+    List<DtCategoria> listarCategorias();
+
+    @WebMethod
+    List<DtCategoria> buscarCategorias();
+
+    @WebMethod
+    List<DtCategoria> buscarCategoriasPorNombre(List<String> nombres);
+
+    // ---------- CIUDADES ---------- //
+    @WebMethod
+    void altaCiudad(DtCiudad ciudad);
+
+    @WebMethod
+    DtCiudad getCiudad(String nombre, String pais);
+
+    @WebMethod
+    List<DtCiudad> listarCiudades();
+
+    // ---------- COMPRAS ---------- //
+    @WebMethod
+    void compraPaquete(DtPaquete paquete, DtCliente cliente);
+
+    // -------- Pasajeros --------- //
+//        List<DtPasajero> listarPasajeros(DtReserva reserva);
+
 }
